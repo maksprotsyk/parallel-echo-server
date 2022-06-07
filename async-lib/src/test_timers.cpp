20fdeead0d00355e9fa7760d65d234127dc802db
ReyBroncas <andriksmen@gmail.com>
2022-06-07 16:28:44 +0300

    added timedEvent

--- async-lib/src/test_timers.cpp
+++ async-lib/src/test_timers.cpp
@@ -5,27 +5,53 @@
 #include <thread>
 
 #include <sys/eventfd.h>
+#include <sys/timerfd.h>
 
-#include "poll_async_handler.h"
 #include "select_async_handler.h"
 #include "epoll_async_handler.h"
+#include "timer_event.h"
 
 using namespace std::chrono_literals;
 
-void runHandler(SelectAsyncHandler* handler) {
+void runHandler(SelectAsyncHandler *handler) {
     handler->runEventLoop();
 }
 
-
-int main(int argc, char *argv[]) {
-    auto* handler = new SelectAsyncHandler();
+void testTimedEvent() {
+    auto *handler = new SelectAsyncHandler();
     std::thread thread(runHandler, handler);
 
+    int fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK);
+    TimerEvent timed_event(handler, fd, 2s, Event::Type::READY_IN);
+
+    timed_event.setCallback([]() { std::cout << "SUCCESS" << std::endl; });
+    timed_event.setTimeoutCallback([]() { std::cout << "TIMEOUT" << std::endl; });
+    timed_event.schedule();
+
+    std::this_thread::sleep_for(2200ms);
+
+    uint64_t res;
+    int ret = read(fd, &res, sizeof(res));
+    if (ret != -1) {
+        printf("timer expired: ret = %d, exp. times = %lu\n", ret, res);
+    }
+
+    handler->finish();
+    thread.join();
+
+    delete handler;
+
+    std::cout << "Finished" << std::endl;
+}
+
+void testEvent() {
+    auto *handler = new SelectAsyncHandler();
+    std::thread thread(runHandler, handler);
 
     int fd = eventfd(0, EFD_NONBLOCK);
     Event timed_event(handler, fd, Event::Type::READY_IN);
-    timed_event.setCallback([](){std::cout << "SUCCESS" << std::endl;});
-    timed_event.setTimeoutCallback([](){std::cout << "TIMEOUT" << std::endl;});
+    timed_event.setCallback([]() { std::cout << "SUCCESS" << std::endl; });
+    timed_event.setTimeoutCallback([]() { std::cout << "TIMEOUT" << std::endl; });
 
     timed_event.schedule(900ms);
 
@@ -33,16 +59,17 @@ int main(int argc, char *argv[]) {
 
     uint64_t x = 1;
     write(fd, &x, sizeof(uint64_t));
-
     handler->finish();
     thread.join();
-
 
     delete handler;
 
     std::cout << "Finished" << std::endl;
 
+}
 
-
+int main(int argc, char *argv[]) {
+    testTimedEvent();
+    testEvent();
     return 0;
 }
